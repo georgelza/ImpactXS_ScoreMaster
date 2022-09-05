ddd7d0fe93322d89de8f905b6e369bebc9a921f4
George Leonard <georgelza@gmail.com>
2022-09-05 17:05:32 +0200

    removed class, simplified a bit

--- main.py
+++ main.py
@@ -32,44 +32,50 @@ global myevent_list
 global myshooter_list
 global currentRowIndex
 
+# Our lists.... Possible collapse into one list.
 myevent_list    = []
 myshooter_list  = []
 
 
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
-
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
-
-        fileMenu = Menu(menu, tearoff=0)
-        fileMenu.add_command(label="New Events")
-        fileMenu.add_command(label="Load Events")
-        fileMenu.add_command(label="Shooters")
-        fileMenu.add_command(label="Scores")
-        fileMenu.add_separator()
-        fileMenu.add_command(label="Exit", command=self.exitProgram)
-        menu.add_cascade(label="File", menu=fileMenu)
-
-        helpMenu = Menu(menu, tearoff=0)
-        helpMenu.add_command(label="Welcome")
-        helpMenu.add_command(label="About...")
-        menu.add_cascade(label="Help", menu=helpMenu)
-
-    def exitProgram(self):
-        exit()
-
 root = Tk()
+root.title('ImpactXS - ScoreMaster')
 
-app = Window(root)
-root.wm_title("ImpactXS - Score Master")
+# Our Functions
+def exitProgram():
+    exit()
+
+def new_event():
+    hide_all_frames()
+    load_events_frame.pack(fill="both", expand=1)
+
+def hide_all_frames():
+    canvas.grid_forget()
+    load_events_frame.pack_forget()
+
+
+# Lets Build the Menu Structure
+menu = Menu(root)
+root.config(menu=menu)
+
+fileMenu = Menu(menu, tearoff=0)
+fileMenu.add_command(label="New Events", command=new_event)
+fileMenu.add_command(label="Load Events")
+fileMenu.add_command(label="Shooters")
+fileMenu.add_command(label="Scores")
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit", command=exitProgram)
+menu.add_cascade(label="File", menu=fileMenu)
+
+helpMenu = Menu(menu, tearoff=0)
+helpMenu.add_command(label="Welcome")
+helpMenu.add_command(label="About...")
+menu.add_cascade(label="Help", menu=helpMenu)
 
 canvas = tkinter.Canvas(root, width=300, height=200)
 canvas.grid(columnspan=5)
 
-# Logos
+
+# Lets add some Logos onto our splash screen
 logo1m      = Image.open('images/1mile.png')
 logo1m      = ImageTk.PhotoImage(logo1m)
 logo_label  = tkinter.Label(image = logo1m)
@@ -87,6 +93,9 @@ logoimpact  = ImageTk.PhotoImage(logoimpact)
 logo_label  = tkinter.Label(image = logoimpact)
 logo_label.image = logoimpact
 logo_label.grid(columnspan=3, column=0, row = 1)
+
+#Create Events Frames
+load_events_frame = Frame(root, width=400, height=400, bg="red")
 
 
 root.mainloop()
