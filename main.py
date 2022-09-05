bc0999a363c6d7d195bc53087da2ba57afee8a7e
George Leonard <georgelza@gmail.com>
2022-09-05 20:08:42 +0200

    Splash screen and meny build

--- main.py
+++ main.py
@@ -26,10 +26,6 @@ __version__     = "0.0.1"
 
 import tkinter
 from tkinter import *
-from tkinter import ttk
-import uuid
-import json
-import datetime
 from PIL import Image, ImageTk
 
 global myevent_list
@@ -40,56 +36,85 @@ global currentRowIndex
 myevent_list    = []
 myshooter_list  = []
 
-root = Tk()
-root.title('ImpactXS - ScoreMaster')
+### Build the SplashScreen
+
+# Create object
+splash_root = Tk()
+
+# Adjust size
+splash_root.geometry("1000x500")
+
+# Set Title
+splash_root.title("ImpactXS - Score Master")
+
+# Read the Image
+image = Image.open("images/1mile.png")
+# Resize the image using resize() method
+resize_image1 = image.resize((350, 350))
+img1 = ImageTk.PhotoImage(resize_image1)
+# create label and add resize image
+label1 = Label(image=img1)
+label1.image = img1
+label1.place(x=0, y=0)
+
+# Read the Image
+image = Image.open("images/2mile.png")
+# Resize the image using resize() method
+resize_image2 = image.resize((350, 350))
+img2 = ImageTk.PhotoImage(resize_image2)
+# create label and add resize image
+label2 = Label(image=img2)
+label2.image = img2
+label2.place(x=650, y=0)
+
+# Read the Image
+image = Image.open("images/impactxs.png")
+# Resize the image using resize() method
+resize_impactxs = image.resize((1000, 150))
+impactxs = ImageTk.PhotoImage(resize_impactxs)
+# create label and add resize image
+labelimpactxs = Label(image=impactxs)
+labelimpactxs.image = impactxs
+labelimpactxs.place(x=0, y=350)
 
 # Our Functions
 def exitProgram():
     exit()
 
+def main():
+    # destroy splash window
+    splash_root.destroy()
+
+    # Execute tkinter
+    root = Tk()
+
+    # Adjust size
+    root.geometry("400x400")
+
+    # Set Title
+    root.title("ImpactXS - Score Master")
+
+    # Lets Build the Menu Structure
+    menu = Menu(root)
+    root.config(menu=menu)
+
+    fileMenu = Menu(menu, tearoff=0)
+    fileMenu.add_command(label="New Event")
+    fileMenu.add_command(label="Load Event")
+    fileMenu.add_command(label="Shooters")
+    fileMenu.add_command(label="Scores")
+    fileMenu.add_separator()
+    fileMenu.add_command(label="Exit", command=exitProgram)
+    menu.add_cascade(label="File", menu=fileMenu)
+
+    helpMenu = Menu(menu, tearoff=0)
+    helpMenu.add_command(label="Welcome")
+    helpMenu.add_command(label="About...")
+    menu.add_cascade(label="Help", menu=helpMenu)
 
 
+# Set the Interval
+splash_root.after(3000, main)
 
-# Lets Build the Menu Structure
-menu = Menu(root)
-root.config(menu=menu)
-
-fileMenu = Menu(menu, tearoff=0)
-fileMenu.add_command(label="New Event")
-fileMenu.add_command(label="Load Event")
-fileMenu.add_command(label="Shooters")
-fileMenu.add_command(label="Scores")
-fileMenu.add_separator()
-fileMenu.add_command(label="Exit", command=exitProgram)
-menu.add_cascade(label="File", menu=fileMenu)
-
-helpMenu = Menu(menu, tearoff=0)
-helpMenu.add_command(label="Welcome")
-helpMenu.add_command(label="About...")
-menu.add_cascade(label="Help", menu=helpMenu)
-
-canvas = tkinter.Canvas(root, width=300, height=200)
-canvas.grid(columnspan=5)
-
-
-# Lets add some Logos onto our splash screen
-logo1m      = Image.open('images/1mile.png')
-logo1m      = ImageTk.PhotoImage(logo1m)
-logo_label  = tkinter.Label(image = logo1m)
-logo_label.image = logo1m
-logo_label.grid(column=0, row = 0)
-
-logo2m      = Image.open('images/2mile.png')
-logo2m      = ImageTk.PhotoImage(logo2m)
-logo_label  = tkinter.Label(image = logo2m)
-logo_label.image = logo2m
-logo_label.grid(column=1, row = 0)
-
-logoimpact  = Image.open('images/impactxs.png')
-logoimpact  = ImageTk.PhotoImage(logoimpact)
-logo_label  = tkinter.Label(image = logoimpact)
-logo_label.image = logoimpact
-logo_label.grid(columnspan=3, column=0, row = 1)
-
-
-root.mainloop()
+# Execute tkinter program
+mainloop()
