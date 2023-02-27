d1fa0a2bd5332f501889ded2af87f2acb0582575
George Leonard <georgelza@gmail.com>
2023-02-27 21:26:34 +0200

    Added Makefile, moved exports to .exports

--- run_scoremaster.sh
+++ run_scoremaster.sh
@@ -1,28 +1,6 @@
-#!/bin/bash
-
-export DEBUGLEVEL=3
-export LOGLEVEL=DEBUG
-export ECHOJSON=0
-export SPLASHTIME=1000
-export AUTOREFRESH=5
-
-# tree or flat
-export SCORE_VIEWER=flat
-
-# Colors
-export frame_bg=lightgray
-export label_text_bg=lightgray
-export label_text_fg=black
-export entry_text_bg=white
-export entry_text_fg=black
-export txtfont=arial
-export txtfont_size=14
-export lblfont=Consolas
-export lblfont_size=16
-export lblframefont=Consolas
-export lblframefont_size=16
+. ./.exports
 
 # Consolas or Arial
 # Ghost White or cadet blue
 
-python main.py
+python3 main.py
